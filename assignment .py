edab8d11c3e98f614dba261be2189468e73db306
Narayanrout <65102790+Narayanrout@users.noreply.github.com>
2020-05-10 10:01:24 +0530

    Add files via upload

---
+++ assignment .py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+
+
+
+# In[12]:
+
+
+N=[]
+for i in range(2000,3200):
+    if(i%7==0,i%5!=0):
+        N.append(i)
+
+
+# In[13]:
+
+
+N
+
+
+# In[ ]:
+
+
+
+
